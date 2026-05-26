6797d554d8a9cb157dba9d06a8efa2e28c06b6d7
Bassam <shahoudbassam@gmail.com>
2026-05-26 13:08:18 +0200

    commit

--- src/App.svelte
+++ src/App.svelte
@@ -7,20 +7,20 @@
     tasks = [...tasks, { text: input, done: false }];
     input = "";
   }
+
   function deleteTask(index) {
     tasks = tasks.filter((_, i) => i !== index);
   }
 </script>
+
 <div class="todo-container">
   <h2>Att göra-lista</h2>
+
   <div class="input-group">
-    <input
-      bind:value={input}
-      placeholder="Skriv en uppgift..."
-      on:keydown={(e) => e.key === 'Enter' && addTask()}
-    />
+    <input bind:value={input} placeholder="Skriv en uppgift..." />
     <button on:click={addTask}>Lägg till</button>
   </div>
+
   <ul>
     {#each tasks as task, index}
       <li>
